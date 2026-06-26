dd69128b86ff0cf3305227be904b6a0075b3305c
Roshanal Global <shuddi1962@users.noreply.github.com>
2026-06-26 08:26:19 +0100

    fix: quote Ireland string in IOSS EU countries list

--- src/app/admin/logistics/ioss/page.tsx
+++ src/app/admin/logistics/ioss/page.tsx
@@ -9,7 +9,7 @@ const EU_COUNTRIES = [
   { code: "HR", name: "Croatia" }, { code: "CY", name: "Cyprus" }, { code: "CZ", name: "Czech Republic" },
   { code: "DK", name: "Denmark" }, { code: "EE", name: "Estonia" }, { code: "FI", name: "Finland" },
   { code: "FR", name: "France" }, { code: "DE", name: "Germany" }, { code: "GR", name: "Greece" },
-  { code: "HU", name: "Hungary" }, { code: "IE", name: Ireland }, { code: "IT", name: "Italy" },
+  { code: "HU", name: "Hungary" }, { code: "IE", name: "Ireland" }, { code: "IT", name: "Italy" },
   { code: "LV", name: "Latvia" }, { code: "LT", name: "Lithuania" }, { code: "LU", name: "Luxembourg" },
   { code: "MT", name: "Malta" }, { code: "NL", name: "Netherlands" }, { code: "PL", name: "Poland" },
   { code: "PT", name: "Portugal" }, { code: "RO", name: "Romania" }, { code: "SK", name: "Slovakia" },
